11163a9678389c27e2a4790945ddb74190affb75
Moyosore Elijah <msontop.me@gmail.com>
2023-01-26 15:17:41 -0800

    ...

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from 'react';
+import { useFormik } from 'formik';
 
-const Form = () => {
+const ContactForm = () => {
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <div>Form</div>
-  )
-}
+    <div class='w-[100%]'>
+      <form onSubmit={formik.handleSubmit}>
+      <div class='flex flex-col '>
+      <label class='font-bold mb-1 text-lg' htmlFor="name">Nom</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+        style={{height: '50px', borderRadius:'10px'}}
+      />
+      </div>
 
-export default Form
+      <div class='flex flex-col md:flex-row justify-between my-5'>
+      <div class='flex flex-col w-full md:w-[45%]'>
+      <label class='font-bold mb-1 text-lg' htmlFor="email">E-mail</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        style={{height: '50px', borderRadius:'10px'}}
+      />
+      </div>
+      <div class='flex flex-col w-full md:w-[45%]'>
+      <label class='font-bold mb-1 text-lg' htmlFor="name">pas-de-telephone</label>
+      <input
+        id="phone"
+        name="phone"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.phone}
+        style={{height: '50px', borderRadius:'10px'}}
+      />
+      </div>
+      </div>
+
+
+      <div class='flex flex-col'>
+      <label class='font-bold mb-1 text-lg' htmlFor="message">Message:</label>
+      <textarea
+        id="=message"
+        name="message"
+        type="=text"
+        onChange={formik.handleChange}
+        value={formik.values.message}
+        style={{minHeight: '100px', borderRadius:'10px'}}
+      />
+      </div>
+
+      <div class='flex justify-center'>
+      <button class='bg-white h-[50px] font-bold my-10 p-4 shadow-md' style={{borderRadius:'10px'}} type="submit">NONS VOUS PALIEREE</button>
+      </div>
+    </form>
+    </div>
+  );
+};
+
+export default ContactForm;
+
+
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import ContactForm from '../components/Form'
+import logo from '../assets/school-hacks-logo.png'
+import { FaFacebook,FaTwitter,FaLinkedin } from 'react-icons/fa'
+import {FiMail} from 'react-icons/fi'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div class='h-[93vh] md:h-[90vh] flex items-center justify-center'>
+        <div class='bg-[#B1A1ED] flex w-full md:w-[80vw] h-full md:h-[80vh]'>
+          <div class='hidden md:flex w-[40%] h-[100%] bg-white border-[#B1A1ED] border-2 items-center justify-center'>
+          <div>
+          <div class='flex justify-center items-center w-full pb-5 md:pb-0'><img src={logo} alt="logo" class='logo-size'/></div>
+          <p class='font-semibold text-lg my-4 text-center'>Soyez social avec nous</p>
+          <div class='flex gap-5'>
+            <div><li><a href='https://web.facebook.com/people/Student-Hacks/100089567025384/'><FaFacebook size='3em' color='blue'/></a></li></div>
+            <div><a href='https://twitter.com/StudentHacks0'><FaTwitter size='3em' color='#00acee'/></a></div>
+            <div><a href='/'><FaLinkedin size='3em' color='#0072b1'/></a></div>
+            <div><a href='mailto:contact.studenthacks@gmail.com'><FiMail size='3em' color='black'/></a></div>
+          </div>
+          </div>
+          </div>
+          <div class='w-full md:w-[60%] flex flex-col md:flex-row items-center justify-center'>
+            <div class='w-[80%]'>
+            <h1 class='font-bold text-2xl text-center'>NOUS CONTACTER</h1>
+            <div class='flex justify-center'><ContactForm/></div>
+            </div>
+            <div class='flex md:hidden gap-5 mt-4 '>
+            <div><a href='https://web.facebook.com/people/Student-Hacks/100089567025384/'><FaFacebook size='3em' color='blue'/></a></div>
+            <div><a href='https://twitter.com/StudentHacks0'><FaTwitter size='3em' color='#00acee'/></a></div>
+            <div><a href='/'><FaLinkedin size='3em' color='#0072b1'/></a></div>
+            <div><a href='mailto:contact.studenthacks@gmail.com'><FiMail size='3em' color='black'/></a></div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
