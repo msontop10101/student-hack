0761113fbce5b1f6cda1c0bcd8d232e993dabee5
elijah <msontop.me@gmail.com>
2023-02-02 05:50:01 +0100

    ...

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -20,17 +20,10 @@ const Signup = () => {
             <div class='w-[80%]'>
               <div class='flex justify-center'>
                 <div class='flex md:hidden items-center gap-2'><h1 class='font-bold text-2xl text-center'>S'INSCRIRE</h1><div><BiLogIn size='2em' /></div></div>
-              </div>
+              </div> 
               <div class='flex justify-center'><SignupForm /></div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-        <div class="bg-white px-16 py-14 rounded-md text-center">
-          <h1 class="text-xl mb-4 font-bold text-slate-500">Do you Want Delete</h1>
-          <button class="bg-red-500 px-4 py-2 rounded-md text-md text-white">Cancel</button>
-          <button class="bg-indigo-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold">Ok</button>
         </div>
       </div>
     </>
